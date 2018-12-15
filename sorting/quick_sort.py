9adab6ab978cc4a2f4314c75586c8ca417a32e4a
arindampradhan <arindampradhan10@gmail.com>
2018-12-15 21:02:16 +0530

    quick sort

---
+++ sorting/quick_sort.py
@@ -0,0 +1,22 @@
+# In place
+# O(nlogn) - best
+# O(n^2) - worst
+
+def partition(A, start, end):
+    pivot = A[end]
+    pIndex = start
+    for i in range(start, end - 1):
+        if A[i] <= pivot:
+            A[i], A[pIndex] = A[pIndex], A[i]
+            pIndex = pIndex + 1
+    A[pIndex], A[end] = A[end], A[pIndex]
+    return pIndex
+
+def quick_sort(A, start, end):
+    if start < end:
+        partitionIndex = partition(A, start, end)
+        quick_sort(A, start,partitionIndex - 1)
+        quick_sort(A,partitionIndex+1, end)
+    return A
+
+print(quick_sort([2,7,1,4,5,3,12,0], 0, 7))
